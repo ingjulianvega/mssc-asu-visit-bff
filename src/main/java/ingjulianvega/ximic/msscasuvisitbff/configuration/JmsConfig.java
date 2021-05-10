4596746e430e405a130d9e16fc04d5825540d529
Julian Vega <ingjulianvega@gmail.com>
2021-05-09 21:34:38 -0500

    :zap: WIP JMS connection succesful

--- src/main/java/ingjulianvega/ximic/msscasuvisitbff/configuration/JmsConfig.java
+++ src/main/java/ingjulianvega/ximic/msscasuvisitbff/configuration/JmsConfig.java
@@ -1,15 +1,22 @@
 package ingjulianvega.ximic.msscasuvisitbff.configuration;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
-import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
 
-import org.springframework.jms.support.converter.MessageType;
+import javax.jms.ConnectionFactory;
 
 @Configuration
 public class JmsConfig {
+
+    @Value("${active-mq.user}")
+    private String user;
+
+    @Value("${active-mq.password}")
+    private String password;
 
     public static final String UPDATE_VISIT_QUEUE = "update-visit";
     public static final String UPDATE_SYSTEM_CHECK_QUEUE = "update-system-check";
@@ -19,7 +26,35 @@ public class JmsConfig {
     public static final String UPDATE_REMISSION_QUEUE = "update-remission";
     public static final String UPDATE_DISABILITY_QUEUE = "update-disability";
 
+    @Bean
+    public ConnectionFactory connectionFactory(){
+        ActiveMQConnectionFactory activeMQConnectionFactory  = new ActiveMQConnectionFactory();
+        //activeMQConnectionFactory.setBrokerURL(brokerUrl);
+        //activeMQConnectionFactory.setTrustedPackages(Arrays.asList("com.mailshine.springbootstandaloneactivemq"));
+        activeMQConnectionFactory.setPassword(password);
+        activeMQConnectionFactory.setUser(user);
+        return  activeMQConnectionFactory;
+    }
 
+    @Bean
+    public JmsTemplate jmsTemplate(){
+        JmsTemplate jmsTemplate = new JmsTemplate();
+        jmsTemplate.setConnectionFactory(connectionFactory());
+        jmsTemplate.setPubSubDomain(true);  // enable for Pub Sub to topic. Not Required for Queue.
+        return jmsTemplate;
+    }
+
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(){
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory());
+        factory.setPubSubDomain(true);
+        return factory;
+    }
+
+
+
+/*
     @Bean // Serialize message content to json using TextMessage
     public MessageConverter jacksonJmsMessageConverter(ObjectMapper objectMapper) {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
@@ -27,5 +62,5 @@ public class JmsConfig {
         converter.setTypeIdPropertyName("_type");
         converter.setObjectMapper(objectMapper);
         return converter;
-    }
+    }*/
 }
