8d4d605690735d63ce0587028d31bec95e0954c7
Julian Vega <ingjulianvega@gmail.com>
2021-07-11 18:22:04 -0500

    :sparkles: Error messages

--- src/main/java/ingjulianvega/ximic/msscasuvisitbff/configuration/VisitBffParameters.java
+++ src/main/java/ingjulianvega/ximic/msscasuvisitbff/configuration/VisitBffParameters.java
@@ -11,14 +11,5 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "visit-bff")
 public class VisitBffParameters {
 
-    private String success;
-    private String prefix;
-    private Error error;
-    private String channelId;
-
-    @Getter
-    @Setter
-    public static class Error {
-        private String general;
-    }
+    private String api;
 }
--- src/main/java/ingjulianvega/ximic/msscasuvisitbff/exception/VisitBffException.java
+++ src/main/java/ingjulianvega/ximic/msscasuvisitbff/exception/VisitBffException.java
@@ -1,15 +1,19 @@
 package ingjulianvega.ximic.msscasuvisitbff.exception;
 
-import lombok.Getter;
+import lombok.*;
+import org.springframework.http.HttpStatus;
 
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class VisitBffException extends RuntimeException {
 
-    private final String code;
-
-    public VisitBffException(final String code, final String message) {
-        super(message);
-        this.code = code;
-    }
+    private HttpStatus httpStatus;
+    private String apiCode;
+    private String error;
+    private String message;
+    private String solution;
 }
 
